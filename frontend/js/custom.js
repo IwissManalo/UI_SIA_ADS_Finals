163b4e85b5e2dca3dee230f274b9c6bbf8a7b57c
Ralph Benedict Vicente <163800278+rapvicente@users.noreply.github.com>
2024-05-31 00:44:14 +0800

    EMPLOYEE PORTAL (CRUD)

--- frontend/js/custom.js
+++ frontend/js/custom.js
@@ -30,6 +30,34 @@ function goBack() {
     window.history.back();
 }
 
+function confirmDelete() {
+    if (confirm("Are you sure you want to delete this schedule?")) {
+      alert("Schedule Deleted Completely");
+      // Logic to delete the problem goes here
+    }
+  }
+
+function showNewScheduleForm() {
+    var newScheduleForm = document.getElementById("newScheduleForm");
+    newScheduleForm.style.display = "block";
+  }
+
+  function showNewScheduleForm2() {
+    var newScheduleForm2 = document.getElementById("newScheduleForm2");
+    newScheduleForm2.style.display = "block";
+  }
+  
+  function saveNewSchedule() {
+    var newScheduleForm = document.getElementById("newScheduleForm");
+    newScheduleForm.style.display = "none";
+    alert("Schedule successfully added!");
+  }
+
+  function saveNewSchedule2() {
+    var newScheduleForm = document.getElementById("newScheduleForm2");
+    newScheduleForm.style.display = "none";
+    alert("Schedule Successfully Updated!");
+  }
 $(".owl-carousel").owlCarousel({
     loop: true,
     margin: 20,
